9bc3e34d4887eef308fbdb27548b1af8c68d7031
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-07-05 05:02:47 +0300

    Flesh out websockets a little more

--- src/api.ts
+++ src/api.ts
@@ -3,19 +3,24 @@ import { log } from "./log.js";
 
 const wsServer = new WebSocketServer({ port: 8080 });
 
-const wsConnections: WebSocket[] = [];
+const clients: WebSocket[] = [];
 
 let updateTimer: NodeJS.Timer | null = null;
 
+const sendUpdate = () => {
+  const now = new Date();
+  const message = JSON.stringify({
+    time: now.toISOString(),
+  });
+
+  clients.forEach((sock) => sock.send(message));
+};
+
 // If any clients are connected, send an update every 250 ms.
 const setOrClearInterval = () => {
-  if (wsConnections.length && !updateTimer) {
-    updateTimer = setInterval(() => {
-      wsConnections.forEach((wsSocket) => {
-        wsSocket.send("something");
-      });
-    }, 250);
-  } else if (!wsConnections.length && updateTimer) {
+  if (clients.length && !updateTimer) {
+    updateTimer = setInterval(sendUpdate, 250);
+  } else if (!clients.length && updateTimer) {
     clearInterval(updateTimer);
     updateTimer = null;
   } else {
@@ -24,14 +29,14 @@ const setOrClearInterval = () => {
 };
 
 wsServer.on("connection", (wsSocket) => {
-  wsConnections.push(wsSocket);
+  clients.push(wsSocket);
   setOrClearInterval();
 
   wsSocket.on("error", log.error);
 
   wsSocket.on("close", () => {
-    const index = wsConnections.indexOf(wsSocket);
-    if (index > -1) wsConnections.splice(index, 1);
+    const index = clients.indexOf(wsSocket);
+    if (index > -1) clients.splice(index, 1);
     setOrClearInterval();
   });
 
